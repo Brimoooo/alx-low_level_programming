b5812161b3d8a24df361f80ce5a73b2fde500a5a
Brimoooo <brahimahlikouild@gmail.com>
2023-03-16 17:10:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - main function
  *
- * return : returning 0
+ * return: returning 0
  * /
 
 int main(void)
 {
-	put("\"programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
