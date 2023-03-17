20bd99dfadd12194be5e46776014348d545be850
Brimoooo <brahimahlikouild@gmail.com>
2023-03-16 17:02:23 -0700

    PUTS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <Mstidio.h>
+
+/**
+ * main - main function
+ *
+ * return : returning 0
+ * /
+
+int main(void)
+{
+	put("\"programming is like building a multilingual puzzle);
+	return (0);
+}
