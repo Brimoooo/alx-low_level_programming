46a77fcfea11a0aba308aa5624670ae377e11720
Brimoooo <brahimahlikouild@gmail.com>
2023-03-19 16:56:12 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is 0\n", n, u);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, u);
 }
 return (0);
 }
