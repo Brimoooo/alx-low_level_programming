b3f767b5f7d48ce41482337f0783587d3667ffa9
Brimoooo <brahimahlikouild@gmail.com>
2023-03-20 15:10:28 -0700

    numbers guys

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-print the nubmers decemale from 0 to 10
+ *
+ * Return: always 0 when success
+ */
+int main(void)
+{
+	int i;
+
+	i = 46;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+putchar('\n');
+return (0);
+}
