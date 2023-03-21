8a3391c650790b600f9eaeb17845b7924e37718f
Brimoooo <brahimahlikouild@gmail.com>
2023-03-20 18:40:13 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print a number from 0 to 6
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
